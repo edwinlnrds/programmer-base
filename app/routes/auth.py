13b9d4f8ae0489c78e61845eb963b3223164935d
Leonards03 <edwin.leonards03@gmail.com>
2021-11-10 17:37:36 +0700

    add routes

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -10,14 +10,8 @@ auth = Blueprint('auth', __name__)
 auth_controller = AuthController()
 
 
-@auth.route('/login', methods=['GET', 'POST'])
-def login():
-    view = render_template('pages/login.html', title='Login', form=LoginForm())
-    return make_response(view)
-
-
 @auth.route('/', methods=['POST'])
-def authenticate():
+def authenticate(): 
     try:
         auth_controller.authenticate(request=request)
         return redirect(url_for('base'))
@@ -25,24 +19,44 @@ def authenticate():
         flash(f'{e}', 'danger')
         return redirect(request.referrer)
 
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm(request.form)
+    view = render_template('pages/login.html', title='Login', form=form)
+    return make_response(view)
+
 
 @auth.route('/register', methods=['GET', 'POST'])
 def register():
     form = CreateAccountForm(request.form)
-    if request.method == 'POST' and form.validate:
+    if request.method == 'POST' and form.validate():
         auth_controller.create_user(request)
         return redirect(url_for('auth.login'))
     else:
         view = render_template(
-            'pages/register.html', title='Register', form=CreateAccountForm())
+            'pages/register.html', title='Register', form=form)
         return make_response(view)
 
 
+@login_required
+@auth.route('/edit-profile')
+def edit_profile():
+    pass
+
+
+@login_required
+@auth.route('/update-password')
 def update_password():
     try:
         auth_controller.update_password(request)
     except Exception as e:
         flash(f'{e}', 'danger')
+
+
+@login_required
+@auth.route('/delete-account')
+def delete_account():
+    pass
 
 
 @login_required
