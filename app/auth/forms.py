ffb523b8c4fee8135832c3be0a5af1ef3c95634f
Leonards03 <edwin.leonards03@gmail.com>
2021-11-10 18:31:15 +0700

    fix: migrate from wtforms 2.3.3 to wtforms 3.0.0

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,22 +1,22 @@
 from flask_wtf import FlaskForm
-from wtforms import TextField, PasswordField, BooleanField, SubmitField
+from wtforms import PasswordField, BooleanField, SubmitField, StringField, EmailField
 from wtforms import validators
 from wtforms.validators import DataRequired, Email, EqualTo
 
 class LoginForm(FlaskForm):
-    username = TextField('Username', id='username', validators=[DataRequired()])
+    username = StringField('Username', id='username', validators=[DataRequired()])
     password = PasswordField('Password', id='password', validators=[DataRequired()])
     remember = BooleanField('Remember me')
 
 class CreateAccountForm(FlaskForm):
-    username = TextField('Username', id='username', validators=[DataRequired()])
-    email = TextField('Email', id='email', validators=[DataRequired(), Email()])
+    username = StringField('Username', id='username', validators=[DataRequired()])
+    email = EmailField('Email', id='email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', id='password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', id='confirm_password', validators=[DataRequired(), EqualTo('password', message='Confirmation password must match password')])
     submit = SubmitField('Register')
 
 class PostForm(FlaskForm):
-    title = TextField('Title', id='title', validators=[DataRequired()])
-    content = TextField('Content', id='content', validators=[DataRequired()])
+    title = StringField('Title', id='title', validators=[DataRequired()])
+    content = StringField('Content', id='content', validators=[DataRequired()])
 
     
