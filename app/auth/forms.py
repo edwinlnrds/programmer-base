318919bd1268ef371cfe520dc12841233c11305d
Leonards03 <edwin.leonards03@gmail.com>
2021-11-10 17:37:51 +0700

    Update forms.py

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,5 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import TextField, PasswordField, BooleanField
+from wtforms import TextField, PasswordField, BooleanField, SubmitField
+from wtforms import validators
 from wtforms.validators import DataRequired, Email, EqualTo
 
 class LoginForm(FlaskForm):
@@ -11,4 +12,11 @@ class CreateAccountForm(FlaskForm):
     username = TextField('Username', id='username', validators=[DataRequired()])
     email = TextField('Email', id='email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', id='password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password', id='confirm_password', validators=[DataRequired(), EqualTo('Password', message='Confirmation password must match password')])
+    confirm_password = PasswordField('Confirm Password', id='confirm_password', validators=[DataRequired(), EqualTo('password', message='Confirmation password must match password')])
+    submit = SubmitField('Register')
+
+class PostForm(FlaskForm):
+    title = TextField('Title', id='title', validators=[DataRequired()])
+    content = TextField('Content', id='content', validators=[DataRequired()])
+
+    
