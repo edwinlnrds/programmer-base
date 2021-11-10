a433c8faad646a16216d5ca5d0f65ad3a2a4b1a4
Leonards03 <edwin.leonards03@gmail.com>
2021-11-10 17:36:52 +0700

    refactor: update db information

--- app/config.py
+++ app/config.py
@@ -10,17 +10,18 @@ class Config(object):
     FLASK_RUN_HOST = env.str('FLASK_RUN_HOST')
     FLASK_RUN_PORT = env.str('FLASK_RUN_PORT')
 
-    DB_HOST = env.str('DB_HOST', 'localhost')
+    DB_HOST = env.str('DB_HOST', '127.0.0.1')
     DB_USERNAME = env.str('DB_USERNAME', 'root')
     DB_PASSWORD = env.str('DB_PASSWORD', '')
-    DB_NAME = env.str('DB_NAME', 'flask_usermanagement')
+    DB_NAME = env.str('DB_NAME', 'programmer_base')
     DB_PORT = env.str('DB_PORT', '3306')
+    MYSQL_UNIX_SOCKET = 'TCP'
 
 
     SQLALCHEMY_TRACK_MODIFICATIONS = True
-    SQLALCHEMY_DATABASE_URI = f"mysql://{DB_USERNAME}{DB_PASSWORD}:@{DB_HOST}/{DB_NAME}"
+    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{DB_USERNAME}{DB_PASSWORD}:@{DB_HOST}/{DB_NAME}"
 
-    SECRET_KEY = env.str('SECRET_KEY')
+    SECRET_KEY = env.str('SECRET_KEY', 'ultimate-secr3t-k3y')
 
     SESSION_PERMANENT = False
     SESSION_TYPE = 'filesystem'
