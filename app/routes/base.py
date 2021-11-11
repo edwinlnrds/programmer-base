23ed3cbb2273dc672edaee7206420f3b900d36f4
Leonards03 <edwin.leonards03@gmail.com>
2021-11-11 12:03:03 +0700

    Update base.py

--- app/routes/base.py
+++ app/routes/base.py
@@ -1,9 +1,12 @@
-from flask import Blueprint, render_template, make_response
+from flask import Blueprint, render_template, make_response, redirect, url_for
+from flask_login import current_user
 
 base = Blueprint('base', __name__)
 
 
 @base.route('/')
 def index():
-    view = render_template('pages/index.html', title='Home')
+    if current_user.is_authenticated:
+        return redirect(url_for('forum.index'))
+    view = render_template('pages/index.html')
     return make_response(view)
